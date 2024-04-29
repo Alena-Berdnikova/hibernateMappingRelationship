15090f1afddb131f4bdb2c8fe6011ae945491488
Alena Berdnikova <berdnikova.alena.sar@gmail.com>
2024-04-29 05:35:54 -0700

    GLAB - 305.5.3B - ManytoMany Relationship and Mapping

--- src/main/java/model/Teacher.java
+++ src/main/java/model/Teacher.java
@@ -4,6 +4,7 @@ import jakarta.persistence.*;
 
 import java.io.Serial;
 import java.io.Serializable;
+import java.util.Set;
 
 @Entity
 @Table
@@ -11,33 +12,53 @@ public class Teacher implements Serializable {
     @Serial
     private static final long serialVersionUID = 1L;
     @Id
-    @GeneratedValue( strategy=GenerationType.IDENTITY )
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int teacherId;
     private String salary;
     private String teacherName;
-    @OneToOne(cascade = CascadeType.ALL)
-    private Address address;
+    /* @OneToOne(cascade = CascadeType.ALL)
+     private Address address;
 
-    public Address getAddress() {
-        return address;
+     public Address getAddress() {
+         return address;
+     }
+
+     public void setAddress(Address address) {
+         this.address = address;
+     }*/
+    @ManyToMany(targetEntity = Cohort.class)
+    private Set<Cohort> cohort;
+
+    public Set<Cohort> getCohort() {
+        return cohort;
     }
 
-    public void setAddress(Address address) {
-        this.address = address;
+    public void setCohort(Set<Cohort> cohort) {
+        this.cohort = cohort;
+    }
+
+    public Teacher(String salary, String teacherName, Set<Cohort> cohort) {
+        this.salary = salary;
+        this.teacherName = teacherName;
+        this.cohort=cohort;
     }
 
     public Teacher(int teacherId, String salary, String teacherName) {
         super();
         this.teacherId = teacherId;
         this.salary = salary;
-        this.teacherName = teacherName; }
-    public Teacher() {}
+        this.teacherName = teacherName;
+    }
+
+    public Teacher() {
+    }
 
     public Teacher(String salary, String teacherName, Department department) {
         super();
         this.teacherName = teacherName;
         this.salary = salary;
     }
+
     public Teacher(String salary, String teacherName) {
         super();
         this.teacherName = teacherName;
@@ -47,19 +68,25 @@ public class Teacher implements Serializable {
     public int getTeacherId() {
         return teacherId;
     }
+
     public void setTeacherId(int teacherId) {
         this.teacherId = teacherId;
     }
+
     public String getSalary() {
         return salary;
     }
+
     public void setSalary(String salary) {
         this.salary = salary;
     }
+
     public String getTeacherName() {
         return teacherName;
     }
+
     public void setTeacherName(String teacherName) {
-        teacherName = teacherName; }
+        teacherName = teacherName;
+    }
 }
 
