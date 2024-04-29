7d8d8508d568858f91cce767aa1e7a03fe1c2867
Alena Berdnikova <berdnikova.alena.sar@gmail.com>
2024-04-29 04:07:57 -0700

    GLAB 305.5.1B - ManyToOne and OneToMany Relationships and Mapping

---
+++ src/main/java/model/Teacher.java
@@ -0,0 +1,54 @@
+package model;
+
+import jakarta.persistence.*;
+
+import java.io.Serial;
+import java.io.Serializable;
+
+@Entity
+@Table
+public class Teacher implements Serializable {
+    @Serial
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue( strategy=GenerationType.IDENTITY )
+    private int teacherId;
+    private String salary;
+    private String teacherName;
+    public Teacher(int teacherId, String salary, String teacherName) {
+        super();
+        this.teacherId = teacherId;
+        this.salary = salary;
+        this.teacherName = teacherName; }
+    public Teacher() {}
+
+    public Teacher(String salary, String teacherName, Department department) {
+        super();
+        this.teacherName = teacherName;
+        this.salary = salary;
+    }
+    public Teacher(String salary, String teacherName) {
+        super();
+        this.teacherName = teacherName;
+        this.salary = salary;
+    }
+
+    public int getTeacherId() {
+        return teacherId;
+    }
+    public void setTeacherId(int teacherId) {
+        this.teacherId = teacherId;
+    }
+    public String getSalary() {
+        return salary;
+    }
+    public void setSalary(String salary) {
+        this.salary = salary;
+    }
+    public String getTeacherName() {
+        return teacherName;
+    }
+    public void setTeacherName(String teacherName) {
+        teacherName = teacherName; }
+}
+
